42a0b34ae25a5e867ed7b521ae52e4ce10ae05fc
theblackjack <rivero_92@outlook.es>
2016-09-21 12:51:27 -0400

    2da fase del paseo del caballo  heuristica

---
+++ Ejercicios Propuestos/7_336_22_PaseoDelCaballo_C/src/PruebaPaseoDelCaballo.java
@@ -0,0 +1,22 @@
+
+/**
+ * @author JARVIS
+ * @fecha 21 de sept. de 2016
+ * @Projecto 7_336_22_PaseoDelCaballo_C
+ * @hora 12:48:47
+ * 
+ */
+
+public class PruebaPaseoDelCaballo 
+{
+	
+	public static void main(String[] args) 
+	{
+		
+		System.out.println("\n\n\t\t===== PASEO DEL CABALLO =====\n");
+		
+		PaseoDelCaballo miPC = new PaseoDelCaballo();
+		miPC.IniciarPaseo();
+	}
+
+}
